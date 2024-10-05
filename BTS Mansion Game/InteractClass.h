789766e070726b646ef9e835c5213797ab7d7ea5
Andrew G <btssd2025@gmail.com>
2024-10-05 15:59:46 -0400

    Worked on the interactclass.h and the interactclass.cpp, just a plan of code

--- BTS Mansion Game/InteractClass.h
+++ BTS Mansion Game/InteractClass.h
@@ -1,6 +1,30 @@
 #pragma once
+#ifndef InteractClass_H
+#define InteractClass_H
+
+#include<iostream>
+#include<string>
+using namespace std;
 class InteractClass
 {
+private:
+	string inputMessage;
+	string outputMessage;
+public:
+	//Constructor
+	InteractClass();
+
+	// Funtion to set input message 
+	void setInputMessage(const string& message);
+	// function to set output message 
+	void setOutputMessage(const string& message);
+	// function for input from player
+	void getUserInput();
+	// function to output message
+	void displayInput();
+	//function to interact with object or statue
+	void runInteraction();
+
 	//Only write declarations here, actual code will be in cpp file.
 };
-
+#endif 
