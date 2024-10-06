a076b64de08af7e6e1ce51cb55641d38f2a12ade
maddy-daniel <madelynd@student.fdu.edu>
2024-10-05 20:42:17 -0400

    Started on the item class

--- BTS Mansion Game/ItemClass.cpp
+++ BTS Mansion Game/ItemClass.cpp
@@ -1,1 +1,57 @@
 #include "ItemClass.h"
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+/*
+Item class gives provides the name and description
+of the item when player picks it up and shown when player
+looks in their inventory.
+*/
+
+class Item
+{
+private: 
+	string name;
+	string description;
+
+public: 
+	Item()
+	{
+		name = " ";
+		description = " ";
+	}
+
+	Item(string n, string des)
+	{
+		name = n;
+		description = des;
+	}
+
+	string getName()
+	{
+		return name;
+	}
+
+	void setName(string newName)
+	{
+		name = newName;
+	}
+
+	string getDescription()
+	{
+		return description;
+	}
+
+	void setDescription(string newDescription)
+	{
+		description = newDescription;
+	}
+
+	void displayItem()
+	{
+		cout << "Item: " << name << endl;
+		cout << "Description: " << description << endl;
+	}
+};
